d72af97b1f6a70210fde25d7e53a5951d3d8b61b
Christian Panadero <panavtec@gmail.com>
2016-07-23 22:19:41 +0100

    Macro to change to test in intellij

--- controllers/usb_usb/keymap_panavtec.c
+++ controllers/usb_usb/keymap_panavtec.c
@@ -7,13 +7,13 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL,BSPC,   INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS, \
          TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,   DEL, END, PGDN,    P7,  P8,  P9,        \
          FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,      ENT,                      P4,  P5,  P6,  PPLS, \
-         FN4 ,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,        UP,           P1,  P2,  P3,        \
+         FN4, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT,        UP,           P1,  P2,  P3,        \
          LCTL,LGUI,LALT,               SPC,                RALT,RGUI,CAPS,RCTL,   LEFT,DOWN,RGHT,    P0,  PDOT, PENT),    \
     /* SpaceFn/media layer */
     KEYMAP(\
          ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,  F12,        PWR, WAKE,SLEP,                         \
-         GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,  F12, DEL,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
-         TRNS,TRNS,FN3 ,ESC ,MUTE,TRNS,TRNS,HOME, UP, END, PSCR,SLCK, PAUS,INS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,      \
+         ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11,  F12, DEL,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,TRNS, \
+         TRNS,TRNS,FN3 ,ESC ,MUTE,FN5 ,TRNS,HOME, UP, END, PSCR,SLCK, PAUS,INS,   TRNS,TRNS,TRNS,    TRNS,TRNS,TRNS,      \
          TRNS,TRNS,TRNS,TRNS,VOLU,TRNS,PGUP,LEFT,DOWN,RGHT,TRNS,TRNS,     TRNS,                      TRNS,TRNS,TRNS,TRNS, \
          TRNS,TRNS,TRNS,TRNS,VOLD,SPC, PGDN,FN1,TRNS, TRNS,APP,           TRNS,        PGUP,         TRNS,TRNS,TRNS,      \
          TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,   HOME,PGDN,END,     TRNS,TRNS,TRNS),     \
@@ -43,11 +43,42 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          LCTL,LALT,LGUI,               SPC,                RALT,RGUI,CAPS,RCTL,   LEFT,DOWN,RGHT,    P0,  PDOT, PENT)     \
 };
 
+/*
+ * Macro definition
+ */
+enum macro_id {
+    TESTS_IN_SPLITTER
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    keyevent_t event = record->event;
+    //uint8_t tap_count = record->tap_count;
+
+    switch (id) {
+        case TESTS_IN_SPLITTER:
+            return (event.pressed ?
+                        MACRO( D(LCTL), D(LSFT), D(LALT), D(A), // Press unsplit all
+                               U(LCTL), U(LSFT), U(LALT), U(A), 
+                               W(100),
+                               D(LALT), D(RBRC), // split
+                               U(LALT), U(RBRC),
+                               W(100),
+                               D(LCTL), D(LSFT), D(T), // go to test
+                               U(LCTL), U(LSFT), U(T),
+                               W(100),
+                               T(ENT), // confirm 1st test imp.
+                               END ) : MACRO( END )
+                    );
+    }
+    return MACRO_NONE;
+}
+
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),     // SpaceFn
     [1] = ACTION_DEFAULT_LAYER_SET(2),   // Mouse layer
     [2] = ACTION_DEFAULT_LAYER_SET(0),   // Back to normal layer
     [3] = ACTION_DEFAULT_LAYER_SET(3),   // Gaming layer
     [4] = ACTION_MODS_ONESHOT(MOD_LSFT), // Oneshot Shift
+    [5] = ACTION_MACRO(TESTS_IN_SPLITTER)
     /*OSX specific bindings -WIP-*/
 };
